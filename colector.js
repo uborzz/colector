fcf432a9d15ed36dde0c6cf95a52071d4e94019a
Marcial Rodríguez <uborzz@gmail.com>
2019-01-05 15:08:04 +0100

    automaticamente carga partidas boton

--- colector.js
+++ colector.js
@@ -1,11 +1,189 @@
-// 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "UborzzBotón";
-button.id = "new-button"
+let continue_token = null;
+let sessionid = null;
+let profileURI = null;
+let tabURIparam = 'matchhistorycompetitive';
 
-var body = document.getElementsByTagName("header")[0];
-body.appendChild(button);
+const maxRetries = 3;
 
-button.addEventListener ("click", function() {
-  alert("did something");
-});
+function getSteamID (minProfile) {
+  return '76' + (parseInt(minProfile) + 561197960265728)
+}
+
+const statusBar = document.createElement('div');
+statusBar.style.margin = '8px 0';
+statusBar.style.whiteSpace = 'pre-wrap';
+statusBar.textContent = "Saludos dama oOoOo caballero."
+const updateStatus = (text, accumulate) => {
+    if (accumulate) {
+        statusBar.textContent = statusBar.textContent + '\n' + text;
+    } else {
+        statusBar.textContent = text;
+    }
+}
+
+const menu = document.createElement('div');
+menu.style.padding = '0 14px';
+menu.id = 'banchecker-menu';
+menu.style.textAlign = 'center';
+
+const createSteamButton = (text, iconURI) => {
+    const button = document.createElement('div');
+    // pullup_item class style replication using js
+    // TODO: move to separate css file for sanity
+    button.style.display = 'inline-block';
+    button.style.backgroundColor = 'rgba( 103, 193, 245, 0.2 )';
+    button.style.padding = '3px 8px 0px 0px';
+    button.style.borderRadius = '2px';
+    button.style.marginRight = '6px';
+    button.style.cursor = 'pointer';
+    button.style.lineHeight = '18px';
+    button.style.color = '#66c0f4';
+    button.style.fontSize = '11px';
+    button.onmouseover = () => {
+        button.style.backgroundColor = 'rgba( 102, 192, 244, 0.4 )';
+        button.style.color = '#ffffff';
+    }
+    button.onmouseout = () => {
+        button.style.backgroundColor = 'rgba( 103, 193, 245, 0.2 )';
+        button.style.color = '#66c0f4';
+    }
+    const iconEl = document.createElement('div');
+    iconEl.className = 'menu_ico';
+    iconEl.style.display = 'inline-block';
+    iconEl.style.verticalAlign = 'top';
+    iconEl.style.padding = iconURI ? '1px 7px 0 6px' : '1px 8px 0 0';
+    iconEl.style.minHeight = '22px';
+    if (iconURI) {
+        const image = document.createElement('img');
+        image.src = iconURI;
+        image.width = '16';
+        image.height = '16';
+        image.border = '0';
+        iconEl.appendChild(image);
+    }
+    button.appendChild(iconEl);
+    const textNode = document.createTextNode(text);
+    button.appendChild(textNode);
+    return button;
+}
+
+const fetchButton = createSteamButton('Load whole match history');
+fetchButton.onclick = () => {
+    console.log("llamar a fetch");
+    fetchMatchHistory();
+    fetchButton.onclick = () => {
+        updateStatus('This button was already pressed. Reload the page if you want to start over.');
+    }
+}
+
+const sendButton = createSteamButton('Send to uborzz page');
+sendButton.onclick = () => {
+    console.log("hay que mandar");
+}
+
+menu.appendChild(statusBar);
+menu.appendChild(fetchButton);
+menu.appendChild(sendButton);
+
+document.querySelector('#subtabs').insertAdjacentElement('afterend', menu);
+
+
+const fetchMatchHistoryPage = (recursively, page, retryCount) => {
+  document.querySelector('#load_more_button').style.display = 'none';
+  document.querySelector('#inventory_history_loading').style.display = 'block';
+  fetch (`${profileURI}gcpd/730?ajax=1&tab=${tabURIparam}&continue_token=${continue_token}&sessionid=${sessionid}`,
+      {
+          credentials: "include"
+      })
+  .then(res => {
+      if (res.ok) {
+          const contentType = res.headers.get("content-type");
+          if (contentType && contentType.indexOf("application/json") !== -1) {
+              return res.json();
+          } else {
+              return res.text();
+          }
+      } else {
+          throw Error(`Code ${res.status}. ${res.statusText}`);
+      }
+  })
+  .then(json => {
+      if (!json.success) {
+          throw Error('error getting valid JSON in response to\n' +
+                      `${profileURI}gcpd/730?ajax=1&tab=${tabURIparam}&continue_token=${continue_token}&sessionid=${sessionid}`);
+      }
+      if (json.continue_token) {
+          continue_token = json.continue_token;
+      } else {
+          updateStatus('No continue_token returned from Steam, looks like there are no more matches to load!');
+          continue_token = null;
+      }
+      const parser = new DOMParser(); // todo: don't create new parser for each request
+      const newData = parser.parseFromString(json.html, 'text/html');
+      let elementsToAppend = '.csgo_scoreboard_root > tbody > tr';
+      let elementToAppendTo = '.csgo_scoreboard_root';
+      if (tabURIparam === 'playerreports' || tabURIparam === 'playercommends') {
+          elementsToAppend = 'tbody > tr';
+          elementToAppendTo = '.generic_kv_table tbody';
+      }
+      newData.querySelectorAll(elementsToAppend).forEach((tr, i) => {
+          if (i > 0) document.querySelector(elementToAppendTo).appendChild(tr);
+      })
+      if (recursively && continue_token) {
+          updateStatus(`Loaded ${page ? page + 1 : 1} page${page ? 's' : ''}...`);
+          fetchMatchHistoryPage(true, page ? page + 1 : 1, maxRetries);
+      } else {
+          updateStatus('Done. ' + page + ' pages loaded. About ' + page*10 + ' games.');
+          if (!continue_token) {
+              document.querySelector('#inventory_history_loading').style.display = 'none';
+          } else {
+              document.querySelector('#load_more_button').style.display = 'inline-block';
+              document.querySelector('#inventory_history_loading').style.display = 'none';
+          }
+      }
+  })
+  .catch((error) => {
+      updateStatus(`Error while loading match history:\n${error}` +
+                   `${retryCount !== undefined && retryCount > 0 ? `\n\nRetrying to fetch page... ${maxRetries - retryCount}/3`
+                                                                 : `\n\nCouldn't load data after ${maxRetries} retries :(`}`);
+      if (retryCount > 0) {
+          setTimeout(() => fetchMatchHistoryPage(true, page, retryCount - 1), 3000);
+      }
+      document.querySelector('#load_more_button').style.display = 'inline-block';
+      document.querySelector('#inventory_history_loading').style.display = 'none';
+  })
+}
+
+const fetchMatchHistory = () => {
+  if (continue_token && sessionid && profileURI) {
+      console.log(`First continue token: ${continue_token} | SessionID: ${sessionid} | Profile: ${profileURI}`);
+      updateStatus('Loading Match history...');
+      fetchMatchHistoryPage(true, 1, maxRetries);
+  }
+}
+
+const profileAnchor = document.querySelector('#global_actions .user_avatar');
+if (!profileAnchor) {
+    updateStatus('Error: .user_avatar element was not found');
+    console.log('Error: .user_avatar element was not found')
+}
+profileURI = profileAnchor.href;
+if (!document.querySelector('#load_more_button')) {
+    updateStatus('No "LOAD MORE HISTORY" button is present, seems like there are no more matches');
+}
+const steamContinueScript = document.querySelector('#personaldata_elements_container+script');
+const matchContinueToken = steamContinueScript.text.match(/g_sGcContinueToken = '(\d+)'/);
+if (!matchContinueToken) {
+    updateStatus('Error: g_sGcContinueToken was not found');
+}
+continue_token = matchContinueToken[1];
+    const steamSessionScript = document.querySelector('#global_header+script');
+    const matchSessionID = steamSessionScript.text.match(/g_sessionID = "(.+)"/);
+    if (!matchSessionID) {
+        updateStatus('Error: g_sessionID was not found');
+    }
+sessionid = matchSessionID[1];
+
+console.log("MIERDA")
+console.log("session", sessionid)
+console.log("token", continue_token)
